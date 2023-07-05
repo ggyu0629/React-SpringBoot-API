5df303f80904f748b49c66ca6f06961851000cdb
ggyu0629 <ksg1885@naver.com>
2023-07-05 16:15:19 +0900

    SpringBoot Setting

---
+++ src/main/java/com/bbi/ittwapi/IttwApiApplication.java
@@ -0,0 +1,13 @@
+package com.bbi.ittwapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IttwApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IttwApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bbi/ittwapi/IttwApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bbi.ittwapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IttwApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
